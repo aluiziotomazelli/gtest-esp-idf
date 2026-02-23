ae0c38eb82621247fb9ce6815c38415408d2fbc0
Your Name <aluiziotomazelli@gmail.com>
2026-02-23 15:39:42 -0400

    feat: include esp_err_t add_constrained_err() on headers

---
+++ 02_idf_mocks/include/i_sum.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "esp_err.h"
+
+class ISum
+{
+public:
+    virtual ~ISum() = default;
+    virtual int add(int a, int b) = 0;
+    virtual int add_constrained(int a, int b) = 0;
+    virtual esp_err_t add_constrained_err(int a, inte b, int &result) = 0;
+};
