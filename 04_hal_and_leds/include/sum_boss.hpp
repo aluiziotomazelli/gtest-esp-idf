418e991530eedaecd0a41414b4ac44b99acfaaf0
Your Name <aluiziotomazelli@gmail.com>
2026-02-24 02:43:45 -0400

    feat: SumBoss receives ILedSargent as depency

--- 04_hal_and_leds/include/sum_boss.hpp
+++ 04_hal_and_leds/include/sum_boss.hpp
@@ -1,15 +1,17 @@
 #pragma once
 
+#include "i_led_sargent.hpp"
 #include "i_sum.hpp"
 
 class SumBoss
 {
 public:
-    SumBoss(ISum &sum);
+    SumBoss(ISum &sum, ILedSargent &led_sargent);
     ~SumBoss() = default;
 
     esp_err_t compute(int a, int b, int &result);
 
 private:
     ISum &sum_;
+    ILedSargent &led_sargent_;
 };
--- 04_hal_and_leds/src/sum_boss.cpp
+++ 04_hal_and_leds/src/sum_boss.cpp
@@ -1,12 +1,22 @@
 #include "sum_boss.hpp"
 
-SumBoss::SumBoss(ISum &sum)
+SumBoss::SumBoss(ISum &sum, ILedSargent &led_sargent)
     : sum_(sum)
+    , led_sargent_(led_sargent)
 {
 }
 
 esp_err_t SumBoss::compute(int a, int b, int &result)
 {
     esp_err_t ret = sum_.add_constrained_err(a, b, result);
+    if (ret == ESP_OK) {            // no error
+        ret = led_sargent_.green(); // check if green led works
+        if (ret != ESP_OK) {        // green led does not work
+            return ret;             // return the error
+        }
+    }
+    else {                  // if the sum failed
+        led_sargent_.red(); // the sum error is already in ret
+    }
     return ret;
 }
